022f0839cc8139d12b5cd716852c764ce4821bc1
sanduyyyyyyyy <24430324_sandy_a@widyadharma.ac.id>
2026-09-02 20:35:41 +0700

    on progress

---
+++ tugas2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nama="aLiF bUdI";
+    $nama1=strtolower($nama);
+    $nama2=ucwords($nama1);
+    echo $nama2; 
+    
+
+
+
+    ?>
+    
+</body>
+</html>
